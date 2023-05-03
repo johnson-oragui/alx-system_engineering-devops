088f2da9e066efd6fb395cc797d142b029644efa
sentense1 <3xsaint@gmail.com>
2023-05-03 07:05:49 -0700

    regulkar

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/Hbttn/).join
+puts ARGV[0].scan(/Hbtttn/).join
+puts ARGV[0].scan(/Hbttttn/).join
+puts ARGV[0].scan(/Hbtttttn/).join
---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/Htn/).join
+puts ARGV[0].scan(/Hbtn/).join
