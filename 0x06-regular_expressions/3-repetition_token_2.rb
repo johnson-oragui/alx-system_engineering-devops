6fbbf5fca08c4ed3d7d4830b64780b162bef8975
sentense1 <3xsaint@gmail.com>
2023-05-03 08:22:13 -0700

    3-repetition_token_2.rb

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbtn/).join
-puts ARGV[0].scan(/hbttn/).join
-puts ARGV[0].scan(/hbtttn/).join
-puts ARGV[0].scan(/hbttttn/).join
+puts ARGV[0].scan(/hbt+n/).join
