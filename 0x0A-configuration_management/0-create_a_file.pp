2457c15faeb56cc3ca607177c0f0d4d15dfff56e
sentense1 <3xsaint@gmail.com>
2023-06-24 01:32:41 -0700

    task 0 and 2

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,10 @@
+# Create a file in /tmp
+
 file { '/tmp/school':
   ensure  => file,
   content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  path    => '/tmp/school',
 }
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Kills a process named killmenow
+
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  path        => '/bin:/usr/bin:/sbin:/usr/sbin',
+  refreshonly => true,
+}
