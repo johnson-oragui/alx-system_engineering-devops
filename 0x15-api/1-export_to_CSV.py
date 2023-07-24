428cd2f0cb705b4fbaa7ca846335920adbdbc80e
sentense1 <3xsaint@gmail.com>
2023-07-24 14:21:20 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,32 +5,35 @@ Exports to-do list information for a given employee ID to CSV format.
 This script takes an employee ID as a command-line argument and exports
 the corresponding user information and to-do list to a CSV file.
 """
-
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    # Get the employee ID from the command-line argument
-    user_id = sys.argv[1]
 
+if __name__ == "__main__":
     # Base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetch user information using the provided employee ID
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
+    # Get the employee information using the provided employee ID
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
 
-    # Fetch the to-do list for the employee using the provided employee ID
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    # Get the to-do list for the employee using the provided employee ID
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params=params).json()
 
     # Create a CSV file with the employee ID as the filename
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
         # Write header row to the CSV file
-        writer.writerow(["User ID", "Username", "Completed", "Title"])
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
+                        "TASK_TITLE"])
 
         # Write each to-do list item as a row in the CSV file
-        for t in todos:
-            writer.writerow([user_id, username, t.get("completed"), t.get("title")])
+        for todo in todos:
+            writer.writerow([user.get("id"), user.get("username"),
+                            todo.get("completed"), todo.get("title")])
+
+    # Print a success message after writing the CSV
+    print("Data exported to {}.csv".format(employee_id))
