5ef612238c0660b6ae1897a0d7c3bc2062fd6399
sentense1 <3xsaint@gmail.com>
2023-07-24 07:16:11 -0700

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,7 +10,8 @@ import json
 import requests
 
 
-if __name__ == "__main__":
+def fetch_user_data():
+    """Fetch user information and to-do lists for all employees."""
     # Base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/"
 
@@ -18,19 +19,26 @@ if __name__ == "__main__":
     users = requests.get(url + "users").json()
 
     # Create a dictionary containing to-do list information of all employees
-    params = {"userId": u.get("id")}
+    data_to_export = {}
+    for user in users:
+        user_id = user["id"]
+        user_url = url + f"todos?userId={user_id}"
+        todo_list = requests.get(user_url).json()
 
-    data_to_export = {
-        u.get("id"): [
+        data_to_export[user_id] = [
             {
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username"),
             }
-            for t in requests.get(url + "todos", params).json()
+            for todo in todo_list
         ]
-        for u in users
-    }
+
+    return data_to_export
+
+
+if __name__ == "__main__":
+    data_to_export = fetch_user_data()
 
     # Write the data to a JSON file
     with open("todo_all_employees.json", "w") as jsonfile:
