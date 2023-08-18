497b263aca9c22264f7acbecd612a6c6ec5b0cee
sentense1 <3xsaint@gmail.com>
2023-08-18 20:29:38 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,11 +3,11 @@
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
 }->
 
 # Restart Nginx
 exec { 'nginx-restart':
   command => '/etc/init.d/nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d/',
 }
