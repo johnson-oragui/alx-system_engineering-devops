7fd5d784b405fa3140b47b4220bd3ce30a399130
sentense1 <3xsaint@gmail.com>
2023-08-24 11:33:07 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,25 +1,6 @@
-# Using puppet to find out why Apache is returning a 500 error.
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-# Define an exec resource to restart Apache after applying the fix
-exec { 'restart_apache':
-  command     => '/usr/sbin/service apache2 restart',
-  refreshonly => true,
-}
-
-# Define a file resource to update the Apache configuration file
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  content => template('apache/000-default.conf.erb'),
-  require => Exec['restart_apache'],
-}
-
-# Define an Apache vhost template (apache/000-default.conf.erb)
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure => file,
-  source => 'puppet:///modules/apache/000-default.conf',
-}
-
-# Define a service resource to ensure Apache is running
-service { 'apache2':
-  ensure => running,
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
