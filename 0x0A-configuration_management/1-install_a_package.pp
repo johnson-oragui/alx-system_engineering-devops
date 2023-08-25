37418fbbd09fb1bedef58d73819a87830e201a53
sentense1 <3xsaint@gmail.com>
2023-08-25 09:11:55 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
-# Installs puppet-lint
+# install_flask.pp
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.*/dist-packages/Flask-2.1.0.dist-info',
+  require => Package['python3-pip'],
+  path    => '/usr/bin',
 }
