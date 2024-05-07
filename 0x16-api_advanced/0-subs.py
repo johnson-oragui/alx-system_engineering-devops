841a8add30c33cf6e787fd3c2d9c5469009725d0
johnson-oragui <johnson-oragui@gmail.com>
2024-05-07 18:21:26 +0100

    added blank lines, modified to use get method

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,16 @@ Script that queries subscribers on a given Reddit subreddit.
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"}  # noqa
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
+        res_json = response.json()
+        data = res_json.get('data')
+        subscribers = data.get('subscribers')
         return subscribers
     else:
         return 0
