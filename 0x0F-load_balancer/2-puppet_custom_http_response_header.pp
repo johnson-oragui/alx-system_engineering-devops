132559eff72bbb5e9cb840a9e3cd7f297d40908d
Johnson Oragui <johnson.oragui@gmail.com>
2024-06-06 17:45:16 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,4 @@
 # Puppet manifest to configure Nginx and HAProxy
-
 package { 'nginx':
   ensure => installed,
 }
@@ -42,50 +41,4 @@ service { 'nginx':
   ensure  => running,
   enable  => true,
   require => File['/etc/nginx/sites-available/default'],
-}
-
-package { 'haproxy':
-  ensure => installed,
-}
-
-file { '/etc/haproxy/haproxy.cfg':
-  ensure  => present,
-  content => '
-    global
-      log /dev/log local0
-      chroot /var/lib/haproxy
-      user haproxy
-      group haproxy
-      daemon
-
-    defaults
-      mode http
-      log global
-      option httplog
-      option dontlognull
-      option redispatch
-      retries 3
-      timeout http-request 10s
-      timeout queue 1m
-      timeout connect 10s
-      timeout client 1m
-      timeout server 1m
-      timeout http-keep-alive 10s
-      timeout check 10s
-
-    frontend http_front
-      bind *:80
-      default_backend Jtechofficial_tech_backend
-
-    backend Jtechofficial_tech_backend
-      balance roundrobin
-      server server1 54.87.224.2:80 check
-      server server2 54.89.109.20:80 check
-  ',
-}
-
-service { 'haproxy':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/haproxy/haproxy.cfg'],
 }
